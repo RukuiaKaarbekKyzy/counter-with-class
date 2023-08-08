ee9e671fb74feccd5cf8b8d02fa48f2e371df2a6
Rukuia <rukuiakaarbekkyzy@gmail.com>
2023-08-08 12:20:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
+import React , { Component } from 'react';
+import styled from 'styled-components'
+import { BootstrapTest } from './BootstrapTest';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const EmpItem =styled.div `
+padding: 20px;
+margin-bottom: 15px;
+border-radius: 5px;
+box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+a{
+  display:block;
+  margin: 10px 0 10px 0;
+  color:${props => props.active ? 'orange' : 'black'};
 }
 
+input{
+  display:block;
+  margin-top: 10px;
+}
+`;
+
+const Header = styled.h2 
+`
+font-size: 22px;
+`;
+
+export const Button = styled.button 
+`padding: 20px;
+display:block;
+background-color: gold;
+border: 1px solid rgba(0,0,0, .2) ;
+box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
+
+class WhoAmI extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      years:27,
+      text:'+++'
+    }
+  }
+
+  nextYear=()=>{
+    this.setState(state=>({
+years:state.years +1
+    }))
+  }
+
+  render (){ 
+    const  {name,surname,link}=this.props;
+   return(
+    <EmpItem active>
+      <Button onClick={this.nextYear}>{this.state.text}</Button>
+    <Header>My name is {name} ,surname-{surname},age-{this.state.years}</Header> 
+    <a href={link}>My profile</a>
+
+    <form >
+   <span>Введите должность</span>
+   <input type="text" onChange={(e)=>this.commitInputChanges (e, 'some color')} />
+    </form>
+   </EmpItem>
+   )
+  }
+   
+  
+}
+
+const Wrapper =styled.div `
+width:600px;
+margin:80px auto 0 auto ;
+`;
+
+const DymamicGreating =(props)=>{
+  return(
+    <div className={'mb-3 p-3  border border-' + props.color}>
+    {
+   React.Children.map(props.children,child=>{
+    return React.cloneElement(child,{className :'shadow p-3 m-3 border rounded'})
+   })
+    }
+    </div>
+  )
+}
+
+const HelloGreeting =()=>{
+  return(
+    <div style={{'width':'600px','margin': '0 auto' }}>
+      <DymamicGreating 
+      color={'primary'}>
+     <h2>Hello World!</h2>
+     </DymamicGreating>
+    </div>
+  )
+}
+
+
+const Message = (props) => {
+  return (
+      <h2>The counter is {props.counter}</h2>
+  )
+}
+
+class Counter extends Component {
+  state = {
+      counter: 0
+  }
+
+  changeCounter = () => {
+      this.setState(({counter}) => ({
+          counter: counter + 1
+      }))
+  }
+
+  render() {
+      return (
+          <>
+              <button
+                  className={'btn btn-primary'}
+                  onClick={this.changeCounter}>
+                  Click me
+              </button>
+              {this.props.some(this.state.counter)}
+          </>
+      )
+  }
+}
+
+function App() {
+return (
+  <Wrapper>
+
+      <Counter some={counter => (
+          <Message counter={counter}/>
+      )}/>
+
+      <HelloGreeting/>
+      <BootstrapTest
+          left = {
+              <DymamicGreating color={'primary'}>
+                  <h2>This weel was hard</h2>
+                  <h2>Hello world!</h2>
+              </DymamicGreating>
+          }
+          right = {
+              <DymamicGreating color={'primary'}>
+                  <h2>RIGHT!</h2>
+              </DymamicGreating>
+          }
+      />
+
+      <WhoAmI name='John' surname="Smith" link="facebook.com"/>
+      <WhoAmI name='Alex' surname="Shepard" link="vk.com"/>
+  </Wrapper>
+);
+}
 export default App;
